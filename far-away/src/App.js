a5349e0a4321cbf0faa5e8bc19aacc56579447a1
solielmaf <miraftsegaye19@gmail.com>
2024-10-28 11:12:29 +0300

    sort

--- far-away/src/App.js
+++ far-away/src/App.js
@@ -65,10 +65,21 @@ function Form({ onAddItems }) {
   );
 }
 function PackageList({ items, onDelete, OnToggle }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDelete={onDelete}
@@ -77,6 +88,13 @@ function PackageList({ items, onDelete, OnToggle }) {
           />
         ))}
       </ul>
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">sort by input order</option>
+          <option value="description">sort by description</option>
+          <option value="packed">sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
